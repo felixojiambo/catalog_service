853aedd4a5d2f75a0745a44ef96b45f542d267de
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-10 03:43:34 -0700

    config output for commong js

---
+++ dist/models/product.model.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    constructor(name, descritpion, price, stock, id) {
+        this.name = name;
+        this.descritpion = descritpion;
+        this.price = price;
+        this.stock = stock;
+        this.id = id;
+    }
+}
+exports.Product = Product;
